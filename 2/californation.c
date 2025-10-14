e698ecc520ca92d5827fdf4a53ba6e718bb7b816
-xylon <r.smagulov@g.nsu.ru>
2025-10-14 09:14:02 +0700

    commit
    2 lab

---
+++ 2/californation.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
+
+main() {
+    time_t now;
+    struct tm *sp;
+    
+    setenv("TZ", "PST8", 1);
+    tzset();
+    (void) time( &now );
+    printf("Время в Калифорнии:\n");
+    printf("%s", ctime( &now ) );
+
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
+}
